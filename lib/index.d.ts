26212d6eacc15f55abdf7be8402007f7fd3c4010
Quill Zhou <quillzhou@gmail.com>
2023-10-04 18:19:08 +0800

    Update index.d.ts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,28 +1,62 @@
-declare class MiniGl {
-  constructor(
-    canvas: HTMLCanvasElement,
-    width: number,
-    height: number,
-    debug?: boolean
-  );
-  setSize(width: number, height: number): void;
-  setOrthographicCamera(
-    left?: number,
-    right?: number,
-    top?: number,
-    bottom?: number,
-    near?: number,
-    far?: number
-  ): void;
-  render(): void;
-}
+/**
+ * @example
+ * // Create your instance
+ * const gradient = new Gradient()
+ *
+ * // Call `initGradient` with the selector to your canvas
+ * gradient.initGradient('#gradient-canvas')
+ * @link https://whatamesh.vercel.app/
+ */
+class Gradient {
+  /**
+   * Creates an instance of Gradient.
+   *
+   * @memberof Gradient
+   */
+  constructor();
 
-declare class Gradient {
-  constructor(selector: string);
+  /**
+   * Pauses the gradient animation.
+   *
+   * @memberof Gradient
+   */
   pause(): void;
+
+  /**
+   * Plays the gradient animation.
+   *
+   * @memberof Gradient
+   */
   play(): void;
+
+  /**
+   * Initializes the gradient with the provided selector.
+   *
+   * @param {string} selector - The selector to your canvas.
+   * @memberof Gradient
+   */
+  initGradient(selector: string): void;
+
+  /**
+   * Toggles the color at the specified index.
+   *
+   * @param {number} index - The index of the color to toggle.
+   * @memberof Gradient
+   */
   toggleColor(index: number): void;
+
+  /**
+   * Shows the legend for the gradient.
+   *
+   * @memberof Gradient
+   */
   showGradientLegend(): void;
+
+  /**
+   * Hides the legend for the gradient.
+   *
+   * @memberof Gradient
+   */
   hideGradientLegend(): void;
 }
 
